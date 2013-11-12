c7aee7f77b67c177aaf4b5c6606f5db6cd56a9b2
Peter Ryan, Jr <accounts@riaog.com>
2013-11-12 18:12:44 -0500

    updated includes

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 </div>
 <br />
-<div class="footer-text">Copyright <?php echo date("Y"); ?> &copy; <a href="http://phryanjr.com">Peter Ryan, Jr.</a> (and Kevin Granger if he does anything)</div>
-</body>   
+<div class="footer-text"></div>
+</body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@ $dir = ltrim ($dir,'/') . ' | ';
 ?>
 <!doctype html>   
 <head>
-<title><?php $file = 'ihsv.me | ' . $dir . basename($_SERVER['PHP_SELF']); echo substr($file, 0, -4); ?></title>
+<title><?php $file = '**site name** ' . $dir . basename($_SERVER['PHP_SELF']); echo substr($file, 0, -4); ?></title>
 <link rel="stylesheet" id="stylesheet" type="text/css" href="/style.css"/>
 <link rel="shortcut icon" type="image/x-icon" href="" />
 <link rel="apple-touch-icon" href="/icon.png"/>
@@ -24,5 +24,5 @@ $dir = ltrim ($dir,'/') . ' | ';
 <div id="nav" class="nav">
                     <div id="menu" class="menu">
                         <a href="/">Home</a>   |   
-                        <a href="/secure/">Who's at the Chateau</a>
+                        <a href=""></a>
                     </div>
