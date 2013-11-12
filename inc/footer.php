32ebd6421932093467a48db383f107d87ed240a9
Peter Ryan, Jr <accounts@riaog.com>
2013-11-12 17:39:46 -0500

    add includes directory

---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+</div>
+<br />
+<div class="footer-text">Copyright <?php echo date("Y"); ?> &copy; <a href="http://phryanjr.com">Peter Ryan, Jr.</a> (and Kevin Granger if he does anything)</div>
+</body>   
+</html>
---
+++ inc/header.php
@@ -0,0 +1,28 @@
+<?php
+$dir = dirname($_SERVER['PHP_SELF']);
+$dir = ltrim ($dir,'/') . ' | ';
+?>
+<!doctype html>   
+<head>
+<title><?php $file = 'ihsv.me | ' . $dir . basename($_SERVER['PHP_SELF']); echo substr($file, 0, -4); ?></title>
+<link rel="stylesheet" id="stylesheet" type="text/css" href="/style.css"/>
+<link rel="shortcut icon" type="image/x-icon" href="" />
+<link rel="apple-touch-icon" href="/icon.png"/>
+<link rel="publisher" href=""/>
+<meta name="description" content=""/>
+<meta name="keywords" content=""/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="robots" content="index, follow"/>
+<meta name="author" content="" />
+<meta property="og:title" content=""/>
+<meta property="og:type" content="article"/>
+<meta property="og:url" content=""/>
+<meta property="og:description" content=""/>
+</head>
+<body>
+<div id="content">
+<div id="nav" class="nav">
+                    <div id="menu" class="menu">
+                        <a href="/">Home</a>   |   
+                        <a href="/secure/">Who's at the Chateau</a>
+                    </div>
